d7139b39f953713e439f687f3197d5038f9d4d85
mjchen <mjchen@nuvoton.com>
2019-10-29 09:30:04 +0800

    add ISP CAN/RS485/I2C sample
    Change-Id: I86c3984a4572ee4601f29b9d7644fca5af521399

---
+++ SampleCode/ISP/ISP_I2C/main.c
@@ -0,0 +1,101 @@
+/***************************************************************************//**
+ * @file     main.c
+ * @brief    ISP tool main function
+ * @version  0x32
+ * @date     14, June, 2017
+ *
+ * @note
+ * Copyright (C) 2017-2018 Nuvoton Technology Corp. All rights reserved.
+ ******************************************************************************/
+#include <stdio.h>
+#include <string.h>
+#include "targetdev.h"
+#include "i2c_transfer.h"
+
+#define PLLCON_SETTING      CLK_PLLCTL_50MHz_HIRC
+#define PLL_CLOCK           50191800
+#define HCLK_DIV                        1
+
+uint32_t Pclk0;
+uint32_t Pclk1;
+
+
+void SYS_Init(void)
+{
+    SYS_UnlockReg();
+    /* Set XT1_OUT(PF.2) and XT1_IN(PF.3) to input mode */
+    PF->MODE &= ~(GPIO_MODE_MODE2_Msk | GPIO_MODE_MODE3_Msk);
+    /* Enable Internal and External XTAL 12MHz clock */
+    CLK->PWRCTL |= CLK_PWRCTL_HIRCEN_Msk | CLK_PWRCTL_HXTEN_Msk;
+
+    /* Waiting for external XTAL clock ready */
+    while (!(CLK->STATUS & CLK_STATUS_HXTSTB_Msk));
+
+    /* Set core clock as PLL_CLOCK from PLL */
+    CLK->PLLCTL = PLLCON_SETTING;
+
+    while (!(CLK->STATUS & CLK_STATUS_PLLSTB_Msk));
+
+    /* Switch HCLK clock source to PLL */
+    CLK_SetHCLK(CLK_CLKSEL0_HCLKSEL_PLL,CLK_CLKDIV0_HCLK(1));
+
+    //SystemCoreClockUpdate();
+    SystemCoreClock = PLL_CLOCK;
+    CyclesPerUs     = SystemCoreClock / 1000000;  // For SYS_SysTickDelay()
+
+    /* Enable I2C1 clock */
+    CLK_EnableModuleClock(I2C1_MODULE);
+    /* Set I2C1 multi-function pins */
+    SYS->GPH_MFPL = (SYS->GPH_MFPL & ~(SYS_GPH_MFPL_PH1MFP_Msk | SYS_GPH_MFPL_PH0MFP_Msk)) |
+                    (SYS_GPH_MFPL_PH1MFP_I2C1_SDA | SYS_GPH_MFPL_PH0MFP_I2C1_SCL);
+    /* I2C clock pin enable schmitt trigger */
+    PH->SMTEN |= GPIO_SMTEN_SMTEN0_Msk;
+}
+
+int main(void)
+{
+    uint32_t cmd_buff[16];
+    SYS_Init();
+    CLK->AHBCLK |= CLK_AHBCLK_ISPCKEN_Msk;
+    FMC->ISPCTL |= FMC_ISPCTL_ISPEN_Msk;
+    g_apromSize = GetApromSize();
+    GetDataFlashInfo(&g_dataFlashAddr, &g_dataFlashSize);
+    I2C_Init();
+    SysTick->LOAD = 300000 * CyclesPerUs;
+    SysTick->VAL   = (0x00);
+    SysTick->CTRL = SysTick->CTRL | SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_ENABLE_Msk;
+
+    while (1)
+    {
+        if (bI2cDataReady == 1)
+        {
+            goto _ISP;
+        }
+
+        if (SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk)
+        {
+            goto _APROM;
+        }
+
+    }
+
+_ISP:
+
+    while (1)
+    {
+        if (bI2cDataReady == 1)
+        {
+            memcpy(cmd_buff, i2c_rcvbuf, 64);
+            bI2cDataReady = 0;
+            ParseCmd((unsigned char *)cmd_buff, 64);
+        }
+    }
+
+_APROM:
+    SYS->RSTSTS = (SYS_RSTSTS_PORF_Msk | SYS_RSTSTS_PINRF_Msk);
+    FMC->ISPCTL &= ~(FMC_ISPCTL_ISPEN_Msk | FMC_ISPCTL_BS_Msk);
+    SCB->AIRCR = (V6M_AIRCR_VECTKEY_DATA | V6M_AIRCR_SYSRESETREQ);
+
+    /* Trap the CPU */
+    while (1);
+}
---
+++ SampleCode/ISP/ISP_RS485/main.c
@@ -0,0 +1,140 @@
+/***************************************************************************//**
+ * @file     main.c
+ * @brief    ISP tool main function
+ * @version  0x32
+ * @date     14, June, 2017
+ *
+ * @note
+ * Copyright (C) 2017-2018 Nuvoton Technology Corp. All rights reserved.
+ ******************************************************************************/
+#include <stdio.h>
+#include "targetdev.h"
+#include "uart_transfer.h"
+
+
+#define PLLCON_SETTING      CLK_PLLCTL_50MHz_HIRC
+#define PLL_CLOCK           50191800
+#define HCLK_DIV                        1
+
+#define nRTSPin                 (PE12)
+#define REVEIVE_MODE            (0)
+#define TRANSMIT_MODE           (1)
+
+__weak uint32_t CLK_GetPLLClockFreq(void)
+{
+    return PLL_CLOCK;
+}
+
+void SYS_Init(void)
+{
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init System Clock                                                                                       */
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+    WDT->CTL &= ~(WDT_CTL_WDTEN_Msk);
+    WDT->CTL |= (WDT_TIMEOUT_2POW18 | WDT_ALTCTL_RSTDSEL_Pos);
+    /* internal 22.1184MHz RC clock */
+    CLK_EnableXtalRC(CLK_PWRCTL_HIRCEN_Msk | CLK_PWRCTL_HXTEN_Msk);
+    /* Waiting for internal clock ready */
+    CLK_WaitClockReady(CLK_STATUS_HIRCSTB_Msk);
+    /* Switch HCLK clock source to HIRC */
+    CLK_SetHCLK(CLK_CLKSEL0_HCLKSEL_HIRC, CLK_CLKDIV0_HCLK(1));
+    /* Set PLL to power down mode and PLL_STB bit in CLKSTATUS register will be cleared by hardware.*/
+    CLK->PLLCTL |= CLK_PLLCTL_PD_Msk;
+    /* Set PLL frequency */
+    CLK->PLLCTL = PLLCON_SETTING;
+    /* Waiting for clock ready */
+    CLK_WaitClockReady(CLK_STATUS_PLLSTB_Msk);
+    /* Switch HCLK clock source to PLL */
+    CLK_SetHCLK(CLK_CLKSEL0_HCLKSEL_PLL, CLK_CLKDIV0_HCLK(1));
+    /* Update System Core Clock */
+    /* User can use SystemCoreClockUpdate() to calculate PllClock, SystemCoreClock and CycylesPerUs automatically. */
+    SystemCoreClockUpdate();
+    //PllClock        = PLL_CLOCK;                        // PLL
+    //SystemCoreClock = PLL_CLOCK / HCLK_DIV;             // HCLK
+    CyclesPerUs     = SystemCoreClock / 1000000;        // For SYS_SysTickDelay()
+    /* Enable UART module clock */
+    CLK_EnableModuleClock(UART1_MODULE);
+    /* Select UART module clock source */
+    CLK_SetModuleClock(UART1_MODULE, CLK_CLKSEL1_UARTSEL_HIRC, CLK_CLKDIV0_UART(1));
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init I/O Multi-function                                                                                 */
+    /*---------------------------------------------------------------------------------------------------------*/
+    PE->MODE = (PE->MODE & ~(0x3ul << (12 << 1))) | (GPIO_MODE_OUTPUT << (12 << 1));
+    nRTSPin = REVEIVE_MODE;
+    SYS->GPB_MFPL = (SYS->GPB_MFPL & ~(SYS_GPB_MFPL_PB2MFP_Msk)) | SYS_GPB_MFPL_PB2MFP_UART1_RXD;
+    SYS->GPB_MFPL = (SYS->GPB_MFPL & ~(SYS_GPB_MFPL_PB3MFP_Msk)) | SYS_GPB_MFPL_PB3MFP_UART1_TXD;
+}
+
+/*---------------------------------------------------------------------------------------------------------*/
+/* MAIN function                                                                                           */
+/*---------------------------------------------------------------------------------------------------------*/
+
+int main(void)
+{
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+    /* Init System, peripheral clock and multi-function I/O */
+    SYS_Init();
+    /* Init UART to 115200-8n1 */
+    UART_Init();
+    CLK->AHBCLK |= CLK_AHBCLK_ISPCKEN_Msk;
+    FMC->ISPCTL |= FMC_ISPCTL_ISPEN_Msk;
+    g_apromSize = GetApromSize();
+    GetDataFlashInfo(&g_dataFlashAddr, &g_dataFlashSize);
+    SysTick->LOAD = 300000 * CyclesPerUs;
+    SysTick->VAL   = (0x00);
+    SysTick->CTRL = SysTick->CTRL | SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_ENABLE_Msk;//using cpu clock
+
+    while (1)
+    {
+        if ((bufhead >= 4) || (bUartDataReady == TRUE))
+        {
+            uint32_t lcmd;
+            lcmd = inpw(uart_rcvbuf);
+
+            if (lcmd == CMD_CONNECT)
+            {
+                goto _ISP;
+            }
+            else
+            {
+                bUartDataReady = FALSE;
+                bufhead = 0;
+            }
+        }
+
+        if (SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk)
+        {
+            goto _APROM;
+        }
+    }
+
+_ISP:
+
+    while (1)
+    {
+        if (bUartDataReady == TRUE)
+        {
+            bUartDataReady = FALSE;
+            ParseCmd(uart_rcvbuf, 64);
+            NVIC_DisableIRQ(UART1_IRQn);
+            nRTSPin = TRANSMIT_MODE;
+            PutString();
+
+            while ((UART1->FIFOSTS & UART_FIFOSTS_TXEMPTYF_Msk) == 0);
+
+            nRTSPin = REVEIVE_MODE;
+            NVIC_EnableIRQ(UART1_IRQn);
+        }
+    }
+
+_APROM:
+    SYS->RSTSTS = (SYS_RSTSTS_PORF_Msk | SYS_RSTSTS_PINRF_Msk);
+    FMC->ISPCTL &= ~(FMC_ISPCTL_ISPEN_Msk | FMC_ISPCTL_BS_Msk);
+    SCB->AIRCR = (V6M_AIRCR_VECTKEY_DATA | V6M_AIRCR_SYSRESETREQ);
+
+    /* Trap the CPU */
+    while (1);
+}
