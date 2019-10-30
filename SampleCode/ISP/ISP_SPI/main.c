9454ae6b82c01eceaaefdf48486314a50a8adf52
cwweng <cwweng@nuvoton.com>
2019-10-30 14:02:58 +0800

    Add ISP SPI sample code
    Change-Id: I3221e3dc5d3c6820a6143c56ceb5b8cea26cf685

---
+++ SampleCode/ISP/ISP_SPI/main.c
@@ -0,0 +1,143 @@
+/***************************************************************************//**
+ * @file     main.c
+ * @brief    ISP tool main function
+ * @version  2.0.0
+ *
+ * Copyright (C) 2019 Nuvoton Technology Corp. All rights reserved.
+ ******************************************************************************/
+#include <stdio.h>
+#include <string.h>
+#include "targetdev.h"
+#include "spi_transfer.h"
+
+#define PLLCTL_SETTING  CLK_PLLCTL_72MHz_HIRC
+#define PLL_CLOCK       72000000
+
+uint32_t Pclk0;
+uint32_t Pclk1;
+
+__weak uint32_t TIMER_Open(TIMER_T *timer, uint32_t u32Mode, uint32_t u32Freq)
+{
+    uint32_t u32Clk = __HXT; // TIMER_GetModuleClock(timer);
+    uint32_t u32Cmpr = 0UL, u32Prescale = 0UL;
+
+    /* Fastest possible timer working freq is (u32Clk / 2). While cmpr = 2, prescaler = 0. */
+    if (u32Freq > (u32Clk / 2UL))
+    {
+        u32Cmpr = 2UL;
+    }
+    else
+    {
+        u32Cmpr = u32Clk / u32Freq;
+        u32Prescale = (u32Cmpr >> 24);  /* for 24 bits CMPDAT */
+
+        if (u32Prescale > 0UL)
+        {
+            u32Cmpr = u32Cmpr / (u32Prescale + 1UL);
+        }
+    }
+
+    timer->CTL = u32Mode | u32Prescale;
+    timer->CMP = u32Cmpr;
+    return (u32Clk / (u32Cmpr * (u32Prescale + 1UL)));
+}
+
+void TIMER3_Init(void)
+{
+    /* Enable IP clock */
+    CLK->APBCLK0 |= CLK_APBCLK0_TMR3CKEN_Msk;
+    /* Select IP clock source */
+    CLK->CLKSEL1 = (CLK->CLKSEL1 & (~CLK_CLKSEL1_TMR3SEL_Msk)) | CLK_CLKSEL1_TMR3SEL_HXT;
+    // Set timer frequency to 3HZ
+    TIMER_Open(TIMER3, TIMER_PERIODIC_MODE, 3);
+    // Enable timer interrupt
+    TIMER_EnableInt(TIMER3);
+    // Start Timer 3
+    TIMER_Start(TIMER3);
+}
+
+void SYS_Init(void)
+{
+    SYS_UnlockReg();
+
+    /* Enable External XTAL (4~24 MHz) */
+    CLK_EnableXtalRC(CLK_PWRCTL_HXTEN_Msk);
+
+    /* Waiting for 12MHz clock ready */
+    CLK_WaitClockReady( CLK_STATUS_HXTSTB_Msk);
+
+    /* Switch HCLK clock source to HXT */
+    CLK_SetHCLK(CLK_CLKSEL0_HCLKSEL_HXT,CLK_CLKDIV0_HCLK(1));
+
+    /* Set PLL to power down mode and PLL_STB bit in CLKSTATUS register will be cleared by hardware.*/
+    CLK->PLLCTL |= CLK_PLLCTL_PD_Msk;
+
+    /* Set PLL frequency */
+    CLK->PLLCTL = CLK_PLLCTL_84MHz_HXT;
+
+    /* Waiting for clock ready */
+    CLK_WaitClockReady(CLK_STATUS_PLLSTB_Msk);
+
+    /* Switch HCLK clock source to PLL */
+    CLK_SetHCLK(CLK_CLKSEL0_HCLKSEL_PLL,CLK_CLKDIV0_HCLK(1));
+
+    /* Set PCLK = HCLK/2 */
+    CLK->CLKSEL0 |= CLK_CLKSEL0_PCLKSEL_HCLK_DIV2;
+
+    /* Select PCLK as the clock source of SPI1 */
+    CLK_SetModuleClock(SPI1_MODULE, CLK_CLKSEL1_SPI1SEL_PCLK, 0);
+    /* Enable SPI1 peripheral clock */
+    CLK_EnableModuleClock(SPI1_MODULE);
+
+    /* Setup SPI1 multi-function pins */
+    /* SPI1: GPC12=SS0, GPC15=MOSI0, GPD0=MISO0, GPD1=CLK */
+    SYS->GPC_MFPH |= (SYS_GPC_MFPH_PC12MFP_SPI1_SS0 | SYS_GPC_MFPH_PC15MFP_SPI1_MOSI0);
+    SYS->GPD_MFPL |= (SYS_GPD_MFPL_PD0MFP_SPI1_MISO0 | SYS_GPD_MFPL_PD1MFP_SPI1_CLK);
+    /* Enable SPI1 clock pin (PD1) schmitt trigger */
+    PD->SMTEN |= GPIO_SMTEN_SMTEN1_Msk;
+}
+
+int main(void)
+{
+    uint32_t cmd_buff[16];
+    SYS_Init();
+    CLK->AHBCLK |= CLK_AHBCLK_ISPCKEN_Msk;
+    FMC->ISPCTL |= (FMC_ISPCTL_ISPEN_Msk | FMC_ISPCTL_APUEN_Msk);
+    g_apromSize = GetApromSize();
+    GetDataFlashInfo(&g_dataFlashAddr, &g_dataFlashSize);
+    SPI_Init();
+    TIMER3_Init();
+
+    while (1)
+    {
+        if (bSpiDataReady == 1)
+        {
+            goto _ISP;
+        }
+
+        if (TIMER3->INTSTS & TIMER_INTSTS_TIF_Msk)
+        {
+            goto _APROM;
+        }
+    }
+
+_ISP:
+
+    while (1)
+    {
+        if (bSpiDataReady == 1)
+        {
+            memcpy(cmd_buff, spi_rcvbuf, 64);
+            bSpiDataReady = 0;
+            ParseCmd((unsigned char *)cmd_buff, 64);
+        }
+    }
+
+_APROM:
+    SYS->RSTSTS = (SYS_RSTSTS_PORF_Msk | SYS_RSTSTS_PINRF_Msk);
+    FMC->ISPCTL &= ~(FMC_ISPCTL_ISPEN_Msk | FMC_ISPCTL_BS_Msk);
+    SCB->AIRCR = (V6M_AIRCR_VECTKEY_DATA | V6M_AIRCR_SYSRESETREQ);
+
+    /* Trap the CPU */
+    while (1);
+}
